cbc303ad41f2ced40cf0b89659330cceb4ab7983
suresh <suresh.avlm@gmail.com>
2020-11-08 08:53:44 +0530

    initial check-ins

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const chalk = require('chalk');
+
+console.log(chalk.green('Database connection successful '));
+
+console.log(chalk.red('Databse connection failed'));
+
+console.log(chalk.yellow('Databse disconnected'));
+
+
